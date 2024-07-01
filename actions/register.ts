d25eefcfbbe55d2705e8493dec6a4cc830bc0873
Nabil <nabil.ziani@brightest.be>
2024-07-01 19:01:32 +0200

    middleware + login

--- actions/register.ts
+++ actions/register.ts
@@ -2,12 +2,11 @@
 
 import * as z from 'zod'
 import { RegisterSchema } from '@/schemas'
-import bcrypt from "bcrypt"
+import bcrypt from "bcryptjs"
 import { db } from '@/lib/db'
 import { getUserByEmail } from '@/data/user'
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
-    console.log(values)
     const validatedFields = RegisterSchema.safeParse(values)
 
     if (!validatedFields.success) {
